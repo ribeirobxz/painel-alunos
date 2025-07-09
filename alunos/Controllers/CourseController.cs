ad9c493b4c5f8387b95756ae42d2c9bdf003c862
sn0w <ribeirok427@gmail.com>
2025-07-09 14:23:37 -0300

    do handler exception in course controller

--- alunos/Controllers/CourseController.cs
+++ alunos/Controllers/CourseController.cs
@@ -1,4 +1,6 @@
-﻿using alunos.Model.Course;
+﻿using System.Linq.Expressions;
+using alunos.Model.Answer;
+using alunos.Model.Course;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
@@ -25,26 +27,29 @@ namespace alunos.Controllers
         [HttpPost]
         public async Task<ActionResult<Course>> CreateCourse([FromBody] CreateCourseModel createCourseModel)
         {
-            if (createCourseModel.Name.IsNullOrEmpty())
+            try
             {
-                return BadRequest(new { message = "O nome do curso não pode ser vazio." });
+                if (string.IsNullOrEmpty(createCourseModel.Name) || string.IsNullOrEmpty(createCourseModel.Description)
+                    || string.IsNullOrEmpty(createCourseModel.IconUrl))
+                {
+                    return Ok(new Answer("Os campos do curso não podem estar em branco.", 204));
+                }
+
+                var course = new Course
+                {
+                    Name = createCourseModel.Name,
+                    Description = createCourseModel.Description,
+                    IconUrl = createCourseModel.IconUrl
+                };
+                await _applicationDBContext.Courses.AddAsync(course);
+                await _applicationDBContext.SaveChangesAsync();
+
+                var answer = new Answer<Course>("Curso criado com sucesso", 201, course);
+                return CreatedAtAction(nameof(GetCourse), new { courseId = course.Id }, answer);
+            } catch(Exception e)
+            {
+                return BadRequest(new Answer(e.Message, 500));
             }
-
-            if(createCourseModel.Description.IsNullOrEmpty())
-            {
-                return BadRequest(new { message = "A descrição do curso não pode ser vazia." });
-            }
-
-            var course = new Course
-            {
-                Name = createCourseModel.Name,
-                Description = createCourseModel.Description,
-                IconUrl = createCourseModel.IconUrl
-            };
-            await _applicationDBContext.Courses.AddAsync(course);
-            await _applicationDBContext.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetCourse), new { courseId = course.Id }, course);
         }
 
         [HttpPatch("{courseId}")]
@@ -73,56 +78,80 @@ namespace alunos.Controllers
         }
 
         [HttpGet("{courseId}")]
-        public async Task<ActionResult<Course>> GetCourse(int courseId)
+        public async Task<ActionResult<Course>> GetCourse(Guid courseId)
         {
-            var course = await _applicationDBContext.Courses.FindAsync(courseId);
-            if (course == null)
+            try
             {
-                return NotFound(new { message = "Curso não encontrado com esse Id." });
-            }
+                var course = await _applicationDBContext.Courses.FindAsync(courseId);
+                if (course == null)
+                {
+                    return Ok(new Answer("Nenhum curso foi encontrado com esse id.", 204));
+                }
 
-            return Ok(course);
+                return Ok(new Answer<Course>("Requisição feita com sucesso.", 200, course));
+            }catch (Exception e)
+            {
+                return BadRequest(new Answer(e.Message, 500));
+            }
         }
 
         [HttpGet("{courseId}/classes")]
         public async Task<ActionResult<IEnumerable<CourseClass>>> GetCourseClasses(Guid courseId)
         {
-            var courseClasses = await _applicationDBContext.CourseClasses
-                .Where(course => course.CourseId == courseId)
-                .ToListAsync();
-            if (courseClasses == null || !courseClasses.Any())
+            try
             {
-                return NotFound(new { message = "Nenhuma aula encontrada para este curso." });
-            }
+                var courseClasses = await _applicationDBContext.CourseClasses
+                    .Where(course => course.CourseId == courseId)
+                    .ToListAsync();
+                if (courseClasses == null || !courseClasses.Any())
+                {
+                    return Ok(new Answer("Nenhuma aula encontrada para este curso.", 204));
+                }
 
-            return Ok(courseClasses);
+                return Ok(new Answer<List<CourseClass>>("Requisição feita com sucesso.", 200, courseClasses));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new Answer(e.Message, 500));
+            }
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Course>>> GetAllCourses()
         {
-            var courses = await _applicationDBContext.Courses.ToListAsync();
-            if (courses == null || !courses.Any())
+            try
             {
-                return NotFound(new { message = "Nenhum curso encontrado." });
-            }
+                var courses = await _applicationDBContext.Courses.ToListAsync();
+                if (courses == null || !courses.Any())
+                {
+                    return Ok(new Answer("Nenhum curso foi encontrado.", 204));
+                }
 
-            return Ok(courses);
+                return Ok(new Answer<List<Course>>("Requisição feita com suceso", 200, courses));
+            } catch(Exception e)
+            {
+                return BadRequest(new Answer(e.Message, 500));
+            }
         }
 
         [HttpDelete("{courseId}")]
         public async Task<ActionResult<Course>> DeleteCourse(int couseId)
         {
-            var course = await _applicationDBContext.Courses.FindAsync(couseId);
-            if (course == null)
+            try
             {
-                return NotFound(new { message = "Nenhum curso foi encontrado com esse Id." });
+                var course = await _applicationDBContext.Courses.FindAsync(couseId);
+                if (course == null)
+                {
+                    return Ok(new Answer("Nenhum curso foi encontrado com esse Id.", 204));
+                }
+
+                _applicationDBContext.Courses.Remove(course);
+                await _applicationDBContext.SaveChangesAsync();
+
+                return Ok(new Answer("Curso deletado com sucesso.", 200));
+            } catch(Exception e) {
+                return BadRequest(new Answer(e.Message, 500));
             }
-
-            _applicationDBContext.Courses.Remove(course);
-            await _applicationDBContext.SaveChangesAsync();
-
-            return Ok(new { message = "Curso deletado com sucesso." });
         }
     }
 }
